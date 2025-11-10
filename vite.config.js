3da080c5c224f55e51a4e4a4d0bb7a493ca9028e
codewithna <codewithna73@gmail.com>
2025-11-10 23:45:16 +0530

    Fix Vite base for Netlify

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/ReactWeb/", // 👈 ye zaroor likh, yahi tera repo name hai
+  base: "./", // ✅ Relative paths for Netlify
   plugins: [react(), tailwindcss()],
 })
